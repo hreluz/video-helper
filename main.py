725989754c1ea1c517f4e168ca9de0db4a85429c
Hector <hector.reluz@avantica.net>
2018-10-04 10:13:18 -0500

    Replacing '/' to '_' to avoid folder name problems, also Exception is shown in case of an error

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def take_html(file):
 			json_object = json.loads(was_found)
 			video_title = json_object['video']['title']
 			for progressive in json_object['request']['files']['progressive']:
-				video = video_path + '/' + video_title + '.mp4'
+				video = video_path + '/' + (video_title + '.mp4').replace('/','_')
 				try:
 					if os.path.exists(video):
 						sp.write("> Video %s ----> skipped because already exists" %(video_title))		
@@ -31,8 +31,8 @@ def take_html(file):
 						urllib.URLopener().retrieve(progressive['url'],video)
 						sp.write("> Video %s download complete" %(video_title))				
 					os.remove(file)
-				except:
-					sp.write("############### ERROR in file %s ###############" %(file))
+				except Exception, e:
+					sp.write("############### ERROR in file %s ############### Error: %s" %(file, str(e)))
 				break
 
 def set_videos_folder(file_path,folder_name,type):
