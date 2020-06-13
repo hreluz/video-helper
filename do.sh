ea20b254b75d3c92627934d67fd14481a66bc2b6
Hector <hmreluz@gmail.com>
2020-06-13 16:49:50 +1000

    Updating virtualenv to always use python2.7

--- do.sh
+++ do.sh
@@ -2,7 +2,7 @@
 directory=".env"
 
 if [ ! -d "${directory}" ]; then
-	virtualenv .env && source .env/bin/activate && pip install -r requirements.txt
+	virtualenv -p /usr/bin/python2.7 .env && source .env/bin/activate && pip install -r requirements.txt
 else
 	source .env/bin/activate
 fi
